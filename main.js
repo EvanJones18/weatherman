3bd1a6561fca9c43f5db7f2717d40c26a5f068db
EvanJones18 <48502664+EvanJones18@users.noreply.github.com>
2020-08-20 20:09:25 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = require('./token.json')
 
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -12,6 +13,17 @@ client.on('message', message => {
     let aishaRole = message.member.roles.cache.some(r => r.name === "Not Among Us")
     
     if (aishaRole) {
-        message.channel.send("How's the weather down there?")
+        let i = Math.random() * 7
+        i = Math.floor(i)
+        console.log(i)
+        if (i === 2) {
+            message.channel.send("How's the weather down there?")
+        }
+        
     }
+})
+
+//Fall guys detection
+client.on('message', message => {
+    let game
 })
