556d3df08fcc02694de1bdd47ffbf2255365c57d
EvanJones18 <48502664+EvanJones18@users.noreply.github.com>
2020-08-21 18:58:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,18 +12,39 @@ client.login(token.token)
 client.on('message', message => {
     let aishaRole = message.member.roles.cache.some(r => r.name === "Not Among Us")
     
-    if (aishaRole) {
-        let i = Math.random() * 7
+    let jokeList = [
+        "How's the weather down there?",
+        "You look like you still have a lot of growing up to do.",
+        "I was gonna tell a short joke, but it'll probably go over your head.",
+        "One day you might rule the world, all 5 ft of it.",
+        "You’re the literal definition of down to earth.",
+        "Aisha is oppressed. She's always getting overlooked.",
+        "Why would Aisha be a bad chef? Because the steaks are always too high.",
+        "Make the little things count. Teach Aisha math.",
+        "The key to writing Aisha jokes is keeping them short.",
+        "Aisha shut up you're short.",
+        "I love short people. They're more down to earth.",
+        "You've gotta hand it to short people. They usually can't reach it."
+    ]
+
+    function randomInt(min, max) {
+        let i = Math.random() * (max - min)
         i = Math.floor(i)
-        console.log(i)
-        if (i === 2) {
-            message.channel.send("How's the weather down there?")
+        return(i)
+    }
+
+    if (aishaRole) {
+        let i = randomInt(0, 0)
+        if (i === 0) {
+            joke = jokeList[randomInt(0, jokeList.length)]
+            
+            message.channel.send(joke)
         }
         
     }
 })
 
 //Fall guys detection
-client.on('message', message => {
-    let game
-})
+//client.on('message', message => {
+//    let game
+//})
