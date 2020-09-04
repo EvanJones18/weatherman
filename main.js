c7437ee1166ac769b1d8a21d2818578ef084f34a
EvanJones18 <48502664+EvanJones18@users.noreply.github.com>
2020-09-03 22:02:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -89,13 +89,15 @@ client.on('message', message => {
                         reaction.message.delete()
                         userPoll.itemIds.forEach(itemId => {
                             reaction.message.channel.messages.fetch(itemId).then(msg => msg.delete())
-                        
+
                         })
-                    console.log("Poll closed")
-                    userPoll.printResponses()
+                        //console.log("Poll closed")
+                        userPoll.printResponses()
+                        userPoll.sendResults()
                     }
-                    
-                    for (let i = 0; i < userPoll.itemIds.length; i ++) {
+
+
+                    for (let i = 0; i < userPoll.itemIds.length; i++) {
                         if (reaction.message.id === userPoll.itemIds[i]) {
                             userPoll.setResponse(user.id, i)
                         }
@@ -103,10 +105,12 @@ client.on('message', message => {
                 }
             })
 
-            
+
         }
     }
 })
+
+
 
 class Poll {
     constructor(message) {
@@ -128,7 +132,7 @@ class Poll {
         let titleEmbed = new Discord.MessageEmbed()
             .setAuthor("Poll")
             .setTitle(this.title)
-            .setDescription("Vote for one of the options below. To prevent users from voting multiple times, only your most recent reaction is counted. Reaction counts do not accurately depict vote counts. React with :white_check_mark: to close the poll and send results.")
+            .setDescription("Vote for one of the options below by reacting with ☑. To prevent users from voting multiple times, only your most recent reaction is counted. Reaction counts do not accurately depict vote counts. React with :white_check_mark: to close the poll and send results.")
 
         this.message.channel.send(titleEmbed)
             .then(sent => {
@@ -151,6 +155,8 @@ class Poll {
         })
     }
 
+
+
     setResponse(userId, response) {
 
         if (this.search(this.responses, "id", userId) === false) {
@@ -158,11 +164,12 @@ class Poll {
         }
 
         else if (this.search(this.responses, "id", userId) !== false) {
-            this.responses.push({ id: userId, response: response })
+            
+            this.responses[this.search(this.responses, "id", userId)]["response"] = response
         }
 
         else {
-            this.responses[this.search(this.responses, "id", userId)]["response"] = response
+            this.responses.push({ id: userId, response: response })
         }
 
     }
@@ -173,38 +180,105 @@ class Poll {
 
     sendResults() {
         let votes = []
-        for (let i = 0; i < this.items; i ++) {
+        for (let i = 0; i < this.items.length; i++) {
             votes.push(0)
         }
+        
 
-        for (let i = 0; i < this.responses; i ++) {
-            votes[this.responses.response] += 1
+        for (let i = 0; i < this.responses.length; i++) {
+            votes[this.responses[i].response] += 1
         }
-
+        //console.log(votes)
         let responseEmbed = new Discord.MessageEmbed()
             .setTitle("Poll Results")
             .setDescription(`Results of \"${this.title}\"`)
-            .addFields(
-                function() {
-                    for (let i = 0; i < this.items.length; i ++) {
-                        let item = this.items[i]
-                        let 
+
+        for (let i = 0; i < this.items.length; i++) {
+            let item = this.items[i]
+            let totalResponses = this.responses.length
+            let numVotes = votes[i]
+
+            let percentBarEmojis = [
+                ":white_large_square:",
+                ":orange_square:",
+                ":blue_square:",
+                ":red_square:",
+                ":brown_square:",
+                ":purple_square:",
+                ":green_square:",
+                ":yellow_square:"
+            ]
+            
+            function percentageBar() {
+
+                function randomInt(min, max) {
+                    let n = Math.random() * (max - min)
+                    n = Math.floor(n)
+                    return (n)
+                }
+
+                let color = randomInt(0, percentBarEmojis.length)
+                let percentBar = ""
+                for (let i = 0; i < Math.ceil((numVotes / totalResponses) * 20); i++) {
+                    percentBar += percentBarEmojis[color]
+                }
+
+                for (let i = 0; i < (20 - Math.ceil((numVotes / totalResponses) * 20)); i++) {
+                    percentBar += ":black_large_square:"
+                }
+
+                if (numVotes === 0) {
+                    percentBar = percentBarEmojis[color]
+                    for (let i = 0; i < 19; i++) {
+                        percentBar += ":black_large_square:"
                     }
                 }
-            )
+                //console.log(percentBar)
+                return percentBar
+                
+            }
+            let percentBar2 = percentageBar()
+            
+            responseEmbed.addFields({
+                name: (`${item} - ${Math.floor((numVotes / totalResponses) * 100)}% (${numVotes} votes)`),
+                //name: ((numVotes / totalResponses) * 100),
+                value: percentBar2
+                
+                
+            })
+        }
+
+
+        this.message.channel.send(responseEmbed)
     }
 
     search(object, key, value) {
-        if (object.length === 0) {
-            return false
-        } 
         
-        else if (object.length !== 0) {
+        function isInArray() {
+            let inArray = false
+            for (let i = 0; i < object.length; i++) {
+                if (object[i][key] === value) {
+                    inArray = true
+                }
+            }
+            //console.log(inArray)
+            return inArray
+        }
+        if (isInArray()) {
             for (let i = 0; i < object.length; i++) {
                 if (object[i][key] === value) {
                     return i
                 }
             }
+            //console.log(true)
+        }
+        else if (!isInArray()) {
+            //console.log(false)
+            return false
+        }
+        else if (object.length === 0) {
+            //console.log(false)
+            return false
         }
     }
 
